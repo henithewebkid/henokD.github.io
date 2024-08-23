c2ca3d484b4eb42ee9d1bdc94ef6d0a958006e0e
henithekid <demissiehenok4@gmail.com>
2024-08-23 15:29:47 +0300

    Your commit message

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -91,7 +91,7 @@ const Works = () => {
           projects. Each project is described briefly and includes links to code
           repositories and live demos. These examples demonstrate my ability to
           solve complex problems, work with diverse technologies, and
-          effectively manage projects and some.
+          effectively manage projects and some other.
         </motion.p>
       </div>
 
